8ed11efb811912eed7045fcec4d6c44a58add452
Saroj Maharjan <maharjan.saroj93@gmail.com>
2025-05-19 12:17:13 +0545

    clerk authentication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,35 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import {Route, Routes, useLocation} from 'react-router-dom'
-import Home from './pages/Home';
+import React from "react";
+import Navbar from "./components/Navbar";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 
 const App = () => {
   //to get the owner path
   const isOwnerPath = useLocation().pathname.includes("owner");
   return (
     <div>
-      {!isOwnerPath && <Navbar/>}
-      <div className='min-h-[70vh]'>
+      {/* <header>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header> */}
+      {!isOwnerPath && <Navbar />}
+      <div className="min-h-[70vh]">
         <Routes>
-          <Route path='/' element={<Home/>}/>
-
+          <Route path="/" element={<Home />} />
         </Routes>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
